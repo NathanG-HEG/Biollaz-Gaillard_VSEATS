2018a7799441b19dc28866a93b2c052ffb5b6960
benjamin <benjamin.biollaz@students.hevs.ch>
2021-10-26 13:58:45 +0200

    BLL skel

---
+++ BLL/CourierManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using DataAccessLayer.DBAccesses;
+
+namespace BLL
+{
+    public class CourierManager
+    {
+        private CouriersDB CouriersDb { get; }
+        private DeliveryAreasDB DeliveryAreasDb { get; }
+        private OrdersDB OrdersDb { get; }
+
+        public CourierManager()
+        {
+            CouriersDb = new CouriersDB();
+            DeliveryAreasDb = new DeliveryAreasDB();
+            OrdersDb = new OrdersDB();
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,20 +23,22 @@ namespace ConsoleApp
              * ID of order here, '1'.
              */
             CompositionDB cdb = new CompositionDB();
-            DishesDB ddb = new DishesDB();
+            DishesDB ddb = new DishesDB(Configuration);
             OrdersDB odb = new OrdersDB();
             //cdb.AddComposition(1, 1, 5);
             //cdb.AddComposition(2, 1, 1);
-            List<Composition> compositions = cdb.GetCompositionsByOrder(1);
+            //List<Composition> compositions = cdb.GetCompositionsByOrder(1);
             
-
+            /*
             int total = 0;
             foreach (var c in compositions)
             {
                 Dish dish = ddb.GetDishById(c.ID_Dish);
                 total += dish.Price * c.Quantity;
             }
-            odb.SetTotal(1, total);
+            odb.SetTotal(1, total);*/
+
+            ddb.AddDish(1, "Penne au pesto", 22);
 
         }
     }
