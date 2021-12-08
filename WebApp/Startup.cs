5b4e80cf45bce3eaa5c12d5b965de734ca611cb8
benjamin <benjamin.biollaz@students.hevs.ch>
2021-12-08 09:48:37 +0100

    corrected nathan's session garbage code

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -66,7 +66,7 @@ namespace WebApp
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseSession();
+           // app.UseSession();
 
             app.UseRouting();
 
