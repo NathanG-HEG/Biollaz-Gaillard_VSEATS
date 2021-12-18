4e0515949955076b8b477c6e0faffb1dee6dbd5d
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2021-12-18 22:07:09 +0100

    Added courier Signup and moved javascript code to site.js

---
+++ WebApp/Models/CourierSignUpViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class CourierSignUpViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required, EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PasswordConfirmation { get; set; }
+        [Required]
+        public int PostCode { get; set; }
+    }
+}
--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showPassword() {
+    var x = document.getElementById("pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+function showPassword2() {
+    var x = document.getElementById("pwd2");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
