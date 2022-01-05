796bb331711b68379e6cec5c96e63cac1282cf4b
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-01-05 14:01:28 +0100

    Controller documentation

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
 ﻿using BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using BLL;
 using DTO;
 using Microsoft.AspNetCore.Http;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
+    /// <summary>
+    /// Controller that offers all base functions
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -22,6 +20,15 @@ namespace WebApp.Controllers
         public IUtilities Utilities { get; }
         public IDeliveryAreaManager DeliveryAreaManager { get; set; }
 
+        /// <summary>
+        /// Controller constructor
+        /// </summary>
+        /// <param name="logger">The logger</param>
+        /// <param name="customerManager">The customer manager object</param>
+        /// <param name="restaurantManager">The restaurant manager object</param>
+        /// <param name="courierManager">The courier manager object</param>
+        /// <param name="utilities">The utilities object</param>
+        /// <param name="deliveryAreaManager">The delivery area manager object</param>
         public HomeController(ILogger<HomeController> logger, ICustomerManager customerManager, IRestaurantManager restaurantManager,
                                 ICourierManager courierManager, IUtilities utilities, IDeliveryAreaManager deliveryAreaManager)
         {
@@ -33,33 +40,58 @@ namespace WebApp.Controllers
             DeliveryAreaManager = deliveryAreaManager;
         }
 
+        /// <summary>
+        /// Function to call the default view
+        /// </summary>
+        /// <returns>Create a an HTML page corresponding to a list of restaurants</returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Function to call the privacy page
+        /// </summary>
+        /// <returns>Create a an HTML page corresponding to a privacy page</returns>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Function that is called in case of error
+        /// </summary>
+        /// <returns>Create a an HTML page corresponding to a error message</returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        /// <summary>
+        /// Function that log out the user
+        /// </summary>
+        /// <returns>Redirects to Index of Customer controller</returns>
         public IActionResult Logout()
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Index", "Customer");
         }
 
+        /// <summary>
+        /// Function that allows a user to sign up
+        /// </summary>
+        /// <returns>Create a an HTML page corresponding to a sign up page</returns>
         public IActionResult CustomerSignUp()
         {
             return View();
         }
 
+        /// <summary>
+        /// HTTP post to get a customer sign up details
+        /// </summary>
+        /// <param name="customerViewModel">Customer details entered by the user</param>
+        /// <returns>Create a an HTML page corresponding to a login page. Redirects to Sign up in case of error</returns>
         [HttpPost]
         public IActionResult CustomerSignUp(CustomerSignUpViewModel customerViewModel)
         {
@@ -85,12 +117,19 @@ namespace WebApp.Controllers
 
             return View("Login");
         }
-
+        /// <summary>
+        /// Function that allows a user to sign up
+        /// </summary>
+        /// <returns>Create a an HTML page corresponding to a sign up page</returns>
         public IActionResult CourierSignUp()
         {
             return View();
         }
-
+        /// <summary>
+        /// HTTP post to get a courier sign up details
+        /// </summary>
+        /// <param name="courierViewModel">Customer details entered by the user</param>
+        /// <returns>Create a an HTML page corresponding to a login page. Redirects to Sign up in case of error</returns>
         [HttpPost]
         public IActionResult CourierSignUp(CourierSignUpViewModel courierViewModel)
         {
@@ -122,11 +161,20 @@ namespace WebApp.Controllers
             return View("Login");
         }
 
+        /// <summary>
+        /// Function to log in
+        /// </summary>
+        /// <returns>Create a an HTML page corresponding to a login page</returns>
         public IActionResult Login()
         {
             return View();
         }
 
+        /// <summary>
+        /// HTTP post to get login information
+        /// </summary>
+        /// <param name="userViewModel">The login details entered by the user</param>
+        /// <returns>Create a an HTML page corresponding to the index corresponding to the type of user signed in</returns>
         [HttpPost]
         public IActionResult Login(UserLoginViewModel userViewModel)
         {
