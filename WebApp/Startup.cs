8dc519df4bbdd94f907b40fcdba64207df93ec14
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-01-05 14:04:12 +0100

    Main doc

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,22 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BLL;
 using BLL.Interfaces;
 using BLL.Managers;
-using DataAccessLayer;
 using DataAccessLayer.DBAccesses;
 using DataAccessLayer.Interfaces;
 
 namespace WebApp
 {
+    /// <summary>
+    /// Auto-generated class
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,7 +23,10 @@ namespace WebApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSession();
@@ -57,7 +57,11 @@ namespace WebApp
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
